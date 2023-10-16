e374d0208952df42f76666a1ecae0c7b2ce30fc7
Asif <asif809999@gmail.com>
2023-10-16 11:27:22 +0600

    gorcery_list.dart added as a list UI 11.4

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,6 +1,21 @@
-class Category{
-  Enum Categories.vegetables
+import 'package:flutter/material.dart';
 
-  final const Category();
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other,
+}
 
+class Category {
+  const Category(this.title, this.color);
+
+  final String title;
+  final Color color;
 }
